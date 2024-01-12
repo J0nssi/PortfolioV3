e09240129076f93af978c5bd5acf33de77e3a555
J0nssi <joonas.vaija@hotmail.com>
2024-01-12 20:43:25 +0200

    pdf fix

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,6 +14,10 @@ import {
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 
+function openPDF() {
+    window.open('/JoonasVaija_CV.pdf') || window.location.replace('/JoonasVaija_CV.pdf');
+}
+
 export default function Nav() {
     return (
         <div className="top-0 fixed flex flex-row justify-end gap-6 items-center bg-gradient-to-b from-background to-transparent w-screen px-4 z-20 xl:flex xl:flex-col xl:h-screen xl:w-auto xl:justify-start xl:top-0 xl:pt-4 xl:bg-gradient-to-b xl:from-transparent xl:to-transparent">
@@ -59,7 +63,7 @@ export default function Nav() {
                         </AlertDialogHeader>
                         <AlertDialogFooter>
                             <AlertDialogCancel>Cancel</AlertDialogCancel>
-                            <AlertDialogAction><a href='/JoonasVaija_CV.pdf' target='_blank'>Continue</a></AlertDialogAction>
+                            <AlertDialogAction onClick={openPDF}>Continue</AlertDialogAction>
                         </AlertDialogFooter>
                     </AlertDialogContent>
                 </AlertDialog>
